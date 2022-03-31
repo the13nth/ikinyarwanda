f56104d141b0e47c74a5bd2d80abe6dc2183af79
the13nth <the13nth@gmail.com>
2022-03-31 08:57:09 +0200

    first commit

---
+++ src/constants/wordlist.ts
@@ -0,0 +1,20 @@
+import { CONFIG } from './config'
+
+export const WORDS = [
+  'inoti',
+]
+
+if (CONFIG.normalization) {
+  WORDS.forEach((val, i) => (WORDS[i] = val.normalize(CONFIG.normalization)))
+}
+
+function shuffle(array: any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+}
+
+if (CONFIG.shuffle) {
+  shuffle(WORDS)
+}
